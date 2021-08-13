8b9a3ef1c316cbae11cb9700a5ea85e888be94ce
Ricardo Balbi <rbalbi@gmail.com>
2021-08-13 18:52:13 -0300

    first commit

---
+++ src/database/migrations/1624554423978-AlterUserAddPassword.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AlterUserAddPassword1624554423978 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({ name: 'password', type: 'varchar', isNullable: true })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'password');
+  }
+}
